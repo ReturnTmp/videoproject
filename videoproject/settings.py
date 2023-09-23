83e6fe33fcf686246b68deb513e641bd1e6f1242
ReturnTmp <99105301+ReturnTmp@users.noreply.github.com>
2023-09-23 19:14:57 +0800

    Update settings.py

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'video',
         'USER': 'root',
-        'PASSWORD': '4643830',
+        'PASSWORD': '111111',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
